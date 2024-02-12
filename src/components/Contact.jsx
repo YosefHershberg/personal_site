a362fdee3402ae3043bf614d98c576666a1f4292
Yosef Hershberg <berioo2000@gmail.com>
2024-02-12 17:02:42 +0200

    added emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,42 @@ const Contact = () => {
     })
   };
 
-  const handleSubmit = (e) => { };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Yosef Herhsberg",
+          from_email: form.email,
+          to_email: "berioo2000@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <div
