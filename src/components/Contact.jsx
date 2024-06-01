279d181ac4015e609f3f5c1c1c8162b1b7f3f2fc
Yosef Hershberg <berioo2000@gmail.com>
2024-06-01 16:01:29 +0300

    small fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl h-fit'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
